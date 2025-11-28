4cfc2822ec1bbf92448454c96b0d5a76f48be03c
FrancescoViadanaLioi <francesco.viadanalioi@gmail.com>
2025-11-27 23:44:49 -0300

    Paciente repository

--- src/repositories/paciente.repository.ts
+++ src/repositories/paciente.repository.ts
@@ -1,21 +1,21 @@
 import Paciente from "../entities/paciente.entity.js"
 
-export async function create(){
-
+export async function create(data: { nome: string; idade: number; telefone: string; convenio: string}){
+    return Paciente.create({data})
 }
 
 export async function findAll(){
-
+    return Paciente.findMany()
 }
 
-export async function findById(){
-    
+export async function findById(id: number){
+    return Paciente.findUnique({where: {id}})
 }
 
-export async function update(){
-
+export async function update(id: number, data: {nome?: string; telefone?: string; convenio?: string}){
+    return Paciente.update({where: {id}, data})
 }
 
-export async function remove(){
-    
+export async function remove(id: number){
+    return Paciente.delete({where: {id}})
 }
