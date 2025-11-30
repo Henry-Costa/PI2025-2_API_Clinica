3d3fbfc6ef8acd8984821ab8e9dea292bd0a5b95
Jéssica Patrocínio <jessicapatrocinio0902@gmail.com>
2025-11-30 08:11:27 -0300

    servicos p profissional atualizados

--- src/services/profissional.service.ts
+++ src/services/profissional.service.ts
@@ -1,25 +1,22 @@
-import * as profissionalRepository from '../repositories/profissional.repository.js'
-export interface Profissional {
-    nome: string;
-    especialidade: string;
-    registroConselho: string;
-}
+import * as profissionalRepository from "../repositories/profissional.repository.js";
+import { Prisma } from "@prisma/client";
 
-export async function createProfissional(data: Profissional) {
-    return await profissionalRepository.create(data)
+export async function createProfissional(data: Prisma.ProfissionalCreateInput) {
+  return await profissionalRepository.create(data);
 }
 
 export async function getAllProfissionais() {
-    return await profissionalRepository.findAll()
+  return await profissionalRepository.findAll();
 }
 
 export async function getProfissionalById(id: number) {
-    return await profissionalRepository.findById(id)
+  return await profissionalRepository.findById(id);
 }
-export async function updateProfissional(id: number, data: Partial<Profissional>) {
-    return await profissionalRepository.update(id, data) 
+
+export async function updateProfissional(id: number, data: Prisma.ProfissionalUpdateInput) {
+  return await profissionalRepository.update(id, data);
 }
 
 export async function deleteProfissional(id: number) {
-    return await profissionalRepository.remove(id)
+  return await profissionalRepository.remove(id);
 }
