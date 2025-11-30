167248cfa02840206865dcc23a3bb9a887e302c6
Jéssica Patrocínio <jessicapatrocinio0902@gmail.com>
2025-11-30 08:12:24 -0300

    mexi no repositorio p profissionais pois o meu create estava retornando null

--- src/repositories/profissional.repository.ts
+++ src/repositories/profissional.repository.ts
@@ -1,21 +1,23 @@
-import Profissional from "../entities/profissional.entity.js"
+import { PrismaClient, Prisma } from "@prisma/client";
 
-export async function create(){
+const prisma = new PrismaClient();
 
+export async function create(data: Prisma.ProfissionalCreateInput) {
+  return await prisma.profissional.create({ data });
 }
 
-export async function findAll(){
-
+export async function findAll() {
+  return await prisma.profissional.findMany();
 }
 
-export async function findById(){
-    
+export async function findById(id: number) {
+  return await prisma.profissional.findUnique({ where: { id } });
 }
 
-export async function update(){
-
+export async function update(id: number, data: Prisma.ProfissionalUpdateInput) {
+  return await prisma.profissional.update({ where: { id }, data });
 }
 
-export async function remove(){
-    
+export async function remove(id: number) {
+  return await prisma.profissional.delete({ where: { id } });
 }
