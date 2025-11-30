5360e58ddadc115674a3d14f17d9407784ff2100
Patrick Dutra <patrickdutra15@gmail.com>
2025-11-30 18:07:08 -0300

    Criado funções de create, update, delete e find no repository de Consulta

--- src/repositories/consulta.repository.ts
+++ src/repositories/consulta.repository.ts
@@ -1,21 +1,23 @@
 import Consulta from "../entities/consulta.entity.js"
 
-export async function create(){
-
+export async function create(data:{nomeProcedimento: string, duracaoHoras: number, duracaoMinutos: number, pacienteId: number, profissionalId: number}){
+    return Consulta.create({data: {nomeProcedimento: data.nomeProcedimento, duracaoHoras: data.duracaoHoras, duracaoMinutos: data.duracaoMinutos, paciente:{
+        connect: {id: data.pacienteId}}, profissional: {connect: {id: data.profissionalId}}} 
+    })
 }
 
 export async function findAll(){
-
+    return Consulta.findMany({include: {paciente: true, profissional: true}})
 }
 
-export async function findById(){
-    
+export async function findById(id: number){
+    return Consulta.findUnique({where: {id}, include: {paciente: true, profissional: true}})
 }
 
-export async function update(){
-
+export async function update(id: number, data:{nomeProcedimento: string, duracaoHoras: number, duracaoMinutos: number, pacienteId: number, profissionalId: number}){
+    return Consulta.update({where: {id}, data})
 }
 
-export async function remove(){
-
+export async function remove(id: number){
+    return Consulta.delete({where: {id}})
 }
