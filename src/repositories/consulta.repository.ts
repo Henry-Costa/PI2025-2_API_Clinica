2d60c69f050dd9fb5d5063563bfe6eaf99b56823
Patrick Dutra <patrickdutra15@gmail.com>
2025-11-30 22:01:46 -0300

    Alteração feita no repository de consulta: implementado INCLUDE, para retornar os dados do paciente e profissionais

--- src/repositories/consulta.repository.ts
+++ src/repositories/consulta.repository.ts
@@ -1,9 +1,14 @@
+import {PrismaClient} from "@prisma/client";
+const prisma = new PrismaClient();
+
 import Consulta from "../entities/consulta.entity.js"
 
 export async function create(data:{nomeProcedimento: string, duracaoHoras: number, duracaoMinutos: number, pacienteId: number, profissionalId: number}){
-    return Consulta.create({data: {nomeProcedimento: data.nomeProcedimento, duracaoHoras: data.duracaoHoras, duracaoMinutos: data.duracaoMinutos, paciente:{
-        connect: {id: data.pacienteId}}, profissional: {connect: {id: data.profissionalId}}} 
-    })
+    return Consulta.create({data: {nomeProcedimento: data.nomeProcedimento, duracaoHoras: data.duracaoHoras, duracaoMinutos: data.duracaoMinutos, paciente:
+        {connect: {id: data.pacienteId}}, profissional: {connect: {id: data.profissionalId}}},
+    include: {paciente: {select: {id: true, nome: true}},
+      profissional: {select: {id: true, nome: true}}}
+  })
 }
 
 export async function findAll(){
